dafaa0fbb3de98ab135334a7bb2beb0ee993a506
Praveen Peruri <praveenperuri@Praveens-MBP.fios-router.home>
2017-07-15 16:13:51 -0500

    final commit

--- FlashCard.js
+++ FlashCard.js
@@ -2,7 +2,61 @@ var BasicCard = require('./BasicCard.js');
 var ClozeCard = require('./ClozeCard.js');
 var fs = require('fs');
 
+function isEmpty(value) {
+    return (value == null || value.length === 0);
+}
 
+function logFlashCard(logText) {
+    fs.appendFile("log.txt", JSON.stringify(logText, null, 2), function (err) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("Flash Card info logged !!");
+        }
+    });
+}
+
+function BasicFlashCard() {
+    var flash = new BasicCard(process.argv[3].replace('"',''), process.argv[4].replace('"',''));
+    logFlashCard(flash);
+}
+
+function ClozeFlashCard() {
+    var flash = new ClozeCard(process.argv[3], process.argv[4]);
+    logFlashCard(flash);
+}
+
+var FlashCard = function () {
+
+    this.storeFlashCard = function () {
+        console.log(process.argv);
+        if (process.argv.length < 3) {
+            console.log('\n Please provide flashcard type Basic/Cloze?'
+                + '\n Followed by "Question" and "Answer" in quotes?'
+                + '\n For Example:'
+                + '\n At command line: node FlashCard.js Basic "Which year was US declared as an independent nation?" "1776"'
+                + '\n or '
+                + '\n At command line: node FlashCard.js Cloze "US was declared as an independent nation in the year 1776" "1776"');
+        } else {
+            if (!isEmpty(process.argv[2]) && !isEmpty(process.argv[3] && !isEmpty(process.argv[4]))) {
+                switch (process.argv[2]) {
+                    case "Basic":
+                        BasicFlashCard();
+                        break;
+                    case "Cloze":
+                        ClozeFlashCard();
+                        break;
+                    default:
+                        console.log('Bad command line arguments !!');
+                        break;
+                }
+            }
+        }
+    };
+};
+
+var fl = new FlashCard();
+fl.storeFlashCard();
 
 // var firstPresident = new BasicCard(
 //     "Who was the first president of the United States?", "George Washington");
